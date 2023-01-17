de412528da3f3c23c0229d91c20a8f6cde59b6ac
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-01-17 22:16:36 +0300

    immutable dog

---
+++ lectures/homework/src/main/java/FinalDog/Dog.java
@@ -0,0 +1,15 @@
+package FinalDog;
+
+final class Dog{
+    private final String name;
+    private final int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void sayHi() {
+        System.out.printf("WOW Dog %s %d y.o.", this.name, this.age);
+    }
+
+}
---
+++ lectures/homework/src/main/java/FinalDog/Main.java
@@ -0,0 +1,9 @@
+package FinalDog;
+
+public class Main {
+    public static void main(String[] args) {
+        var myDog = new Dog("Sharik", 7);
+
+        myDog.sayHi();
+    }
+}
